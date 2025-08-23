ba8a26cf971e8ffe17a64559b1b55d79ed063c34
Zensharp <48300131+zensharp@users.noreply.github.com>
2025-08-23 12:55:54 -0700

    improve issue filtering

--- Tanuki/Core/IssueFilterer.cs
+++ Tanuki/Core/IssueFilterer.cs
@@ -7,11 +7,11 @@ namespace Tanuki.Core
 {
 	public class IssueFilterer
 	{
-		private Regex selectorRegex;
-		private string template;
-		private bool negate;
-		private bool isWildcard;
-		
+		readonly Regex selectorRegex;
+		readonly string template;
+		readonly bool negate;
+		readonly bool isWildcard;
+
 		public IssueFilterer(string filterExpression)
 		{
 			if (filterExpression == "*")
@@ -19,36 +19,40 @@ namespace Tanuki.Core
 				isWildcard = true;
 				return;
 			}
-			
-			var parseRegex = new Regex(@"^(?<left>.+)\s*(?<operator>==|!=)\s*(?<right>.+)$");
+
+			var parseRegex = new Regex(@"(?<left>.+)\s*(?<operator>==|!=)\s*(?<right>.+)");
 			var match = parseRegex.Match(filterExpression);
 			if (!match.Success)
 			{
-				throw new ArgumentException($"Malformed expression: filterExpression");
+				throw new ArgumentException($"Malformed expression: {filterExpression}");
 			}
-			template = match.Groups["left"].Value;
-			selectorRegex = new Regex(match.Groups["right"].Value);
+			template = match.Groups["left"].Value.Trim();
+
+			var right = match.Groups["right"].Value.Trim();
+			selectorRegex = new Regex(right);
 			negate = match.Groups["operator"].Value == "!=";
 		}
-		
+
 		public bool IsMatch(Issue issue)
 		{
 			if (isWildcard)
 			{
 				return true;
 			}
-			
+
 			var text = template;
-			text = text.Replace(".check_name", issue.check_name ?? string.Empty);
 			text = text.Replace(".body", issue.body ?? string.Empty);
 			text = text.Replace(".category", issue.category ?? string.Empty);
+			text = text.Replace(".check_name", issue.check_name ?? string.Empty);
 			text = text.Replace(".description", issue.description ?? string.Empty);
 			text = text.Replace(".fingerprint", issue.fingerprint ?? string.Empty);
 			text = text.Replace(".linter", issue.linter ?? string.Empty);
-			text = text.Replace(".severity", issue.severity ?? string.Empty);
 			text = text.Replace(".location.path", issue.location?.path ?? string.Empty);
-			
+			text = text.Replace(".severity", issue.severity ?? string.Empty);
+
 			return selectorRegex.IsMatch(text) == !negate;
 		}
+
+		public override string ToString() => selectorRegex.ToString();
 	}
 }
